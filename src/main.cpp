4a5eba1e27cdb323cf546f104250c888d51cc6ff
Sztaszko <majneimejl@gmail.com>
2023-02-21 21:52:33 +0100

    Replace queue with stream buffer, change sending buffer to union and rewrite sending mechanism.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "Arduino.h"
 #include "BluetoothSerial.h"
+#include <freertos/stream_buffer.h>
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run 'make menuconfig' to enable it
@@ -15,81 +16,43 @@ const int EMG_CH1 = 36;
 const int EMG_CH2 = 39;
 const int EMG_CH3 = 34;
 const short channels = 3;
-const int QUEUE_SIZE = channels*5000;
 const int sampling_freq = 1000; //Hz
-uint8_t *send_buffer = NULL;
-const TickType_t queue_delay = 1 / portTICK_PERIOD_MS; //block for 1 ms
+const unsigned int send_buffer_size = channels*500;
+
+union {
+  uint8_t bytes[send_buffer_size*sizeof(uint16_t)];
+  uint16_t values[send_buffer_size];
+} send_buffer;
 
 BluetoothSerial SerialBT;
 
-unsigned short int debug_cnt;
-
 TaskHandle_t TaskRead;
 TaskHandle_t TaskSend;
-QueueHandle_t dataQueue;
+StreamBufferHandle_t dataStream;
 
 hw_timer_t *readTimer = NULL;
 
 
-void sendData() {
-//  if (samples_to_send == send_samples_cnt) {
-//    sending = false;
-//    send_samples_cnt = 0;
-//    return;
-//  }
-//
-//  send_samples_cnt++;
-//  xQueueReceive(dataQueue, &value_to_send, queue_delay);
-//  SerialBT.write(value_to_send);
-
-//  uint8_t *samples = (uint8_t*) malloc(samples_to_send*sizeof(uint8_t));
-
-  Serial.print("[DEBUG] samples size: ");
-  Serial.println(samples_to_send);
-  
-  send_buffer = (uint8_t*) realloc(send_buffer, samples_to_send*sizeof(uint16_t));
-  xQueueReceive(dataQueue, send_buffer, queue_delay);
-
-  int bytes_sent;
-  bytes_sent = SerialBT.write(send_buffer, samples_to_send*2);
-
-  Serial.print("[DEBUG] send data bytes sent: ");
-  Serial.println(bytes_sent);
-}
-
-void resendData() {
-  int bytes_sent;
-  bytes_sent = SerialBT.write(send_buffer, samples_to_send*2);
-  Serial.print("[DEBUG] resend data bytes sent: ");
-  Serial.println(bytes_sent);
-}
-
 void IRAM_ATTR readTimerISR() {
-  unsigned short emg_value;
+  uint16_t emg_value;
   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
   if (device_connected) {
     emg_value = analogRead(EMG_CH1);
-//    xQueueSendFromISR(dataQueue, &emg_value, &xHigherPriorityTaskWoken);
-    debug_cnt++;
-    xQueueSendFromISR(dataQueue, &debug_cnt, &xHigherPriorityTaskWoken);
+    xStreamBufferSendFromISR(dataStream, &emg_value, sizeof(emg_value), &xHigherPriorityTaskWoken);
     // switch context if necessary
     if(xHigherPriorityTaskWoken) {
         portYIELD_FROM_ISR();
     }
     
     emg_value = analogRead(EMG_CH2);
-//    xQueueSendFromISR(dataQueue, &emg_value, &xHigherPriorityTaskWoken);
-    debug_cnt++;
-    xQueueSendFromISR(dataQueue, &debug_cnt, &xHigherPriorityTaskWoken);
+    xStreamBufferSendFromISR(dataStream, &emg_value, sizeof(emg_value), &xHigherPriorityTaskWoken);
     if(xHigherPriorityTaskWoken) {
           portYIELD_FROM_ISR();
     }
     
     emg_value = analogRead(EMG_CH3);
-//    xQueueSendFromISR(dataQueue, &emg_value, &xHigherPriorityTaskWoken);
-    debug_cnt++;
-    xQueueSendFromISR(dataQueue, &debug_cnt, &xHigherPriorityTaskWoken);
+    xStreamBufferSendFromISR(dataStream, &emg_value, sizeof(emg_value), &xHigherPriorityTaskWoken);
     if(xHigherPriorityTaskWoken) {
           portYIELD_FROM_ISR();
     }
@@ -98,19 +61,18 @@ void IRAM_ATTR readTimerISR() {
 
 // TODO: change to read from external ADC unit if used
 void taskReadCode(void * pvParameters) {
-   Serial.print("Task Read running on core ");
-   Serial.println(xPortGetCoreID());
-   
+  Serial.print("Task Read running on core ");
+  Serial.println(xPortGetCoreID());
 
-   readTimer = timerBegin(0, 80, true); // prescaler for 1 us
-   timerAttachInterrupt(readTimer, &readTimerISR, true);
-   timerAlarmWrite(readTimer, 1000, true);
-   timerAlarmEnable(readTimer);
-   
-   for(;;) {
-      delay(100);
-   }
-   vTaskDelete(NULL);
+  /* setup timer alarm, but enable when bluetooth connected */
+  readTimer = timerBegin(0, 80, true); // prescaler for 1 us
+  timerAttachInterrupt(readTimer, &readTimerISR, true);
+  timerAlarmWrite(readTimer, 1000000/sampling_freq, true);
+
+  for(;;) {
+    delay(100);
+  }
+  vTaskDelete(NULL);
 }
 
 void taskSendCode(void * pvParameters) {
@@ -123,24 +85,15 @@ void taskSendCode(void * pvParameters) {
 //    Serial.println(messages_no);
 
     if (!device_connected) {
-      // wait for BT connection
-      delay(50);
+      delay(50u);
     } else {
-      if (!sending) {
-        samples_to_send = uxQueueMessagesWaiting(dataQueue);
-//        Serial.print("[DEBUG] samples queue size: ");
-//        Serial.println(samples_to_send);
-        if (samples_to_send == 0)
-          Serial.println("Buffer empty!");
-       else {
-          if (samples_to_send == QUEUE_SIZE)
-            Serial.println("Buffer full!");
-          sending = true;
-          sendData();
-       }
-      } else {
-        delay(5); //sending data, wait for finish
+      //ALERT == send_buffer_size to wait to fill send_buffer
+      if (!congestedBT) {
+        xStreamBufferReceive(dataStream, send_buffer.values, send_buffer_size*sizeof(uint16_t), portMAX_DELAY);
       }
+      Serial.print("Sending: ");
+      Serial.println(send_buffer.values[0]);
+      SerialBT.write(send_buffer.bytes, send_buffer_size*sizeof(uint16_t));
     }
   }
   
@@ -162,28 +115,27 @@ static void BTCallback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
           timerAlarmDisable(readTimer);
           timerStop(readTimer);
           timerWrite(readTimer, 0);
-          xQueueReset(dataQueue);
-          if (send_buffer)
-            free(send_buffer);
+          xStreamBufferReset(dataStream);
           break;
         case ESP_SPP_CONG_EVT:
-          Serial.println("BT congested");
-          if (param->cong.cong == 0) {
-            if (congestedBT) {
-              congestedBT = false;
-              resendData();
-            } else {
-              sendData();
-            }
-          }
+          Serial.println("BT congested event");
+          // if (param->cong.cong == 0) {
+          //   if (congestedBT) {
+          //     congestedBT = false;
+          //     resendData();
+          //   } else {
+          //     sendData();
+          //   }
+          // }
           break;
         case ESP_SPP_WRITE_EVT:
           if (param->write.status == ESP_SPP_SUCCESS) {
             if (param->write.cong == 0) {
-              sendData();
+              congestedBT = false;
             }
           } else {
             if (param->write.len == -1) {
+              Serial.println("Failed to write because of congestion");
               congestedBT = true;
             }
           }
@@ -209,9 +161,11 @@ void setup() {
   
   Serial.println("The device has started");
 
-  dataQueue = xQueueCreate(QUEUE_SIZE, sizeof(unsigned short));
-  if (dataQueue == NULL) {
-    Serial.println("Error creating the data queue");
+  dataStream = xStreamBufferCreate(
+    channels*sizeof(uint16_t)*sampling_freq*5,
+    sizeof(send_buffer.bytes)); /* Alert at the size of send buffer */
+  if (dataStream == NULL) {
+    Serial.println("Error creating the data stream buffer");
   }
   
   xTaskCreatePinnedToCore(
@@ -222,7 +176,7 @@ void setup() {
       1,              /* priority of the task */
       &TaskRead,      /* Task handle to keep track of created task */
       0);             /* pin task to core 0 */
-  delay(500);
+  delay(100);
 
   xTaskCreatePinnedToCore(
       taskSendCode,   /* Task function. */
@@ -232,7 +186,7 @@ void setup() {
       1,              /* priority of the task */
       &TaskSend,      /* Task handle to keep track of created task */
       1);             /* pin task to core 1 */
-  delay(500);
+  delay(100);
 }
 
 void loop() {
